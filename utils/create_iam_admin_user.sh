db2cb373dafd441e8f80f82a726eb9263492b1e2
Yeojin Kim <yeojin.dev@gmail.com>
2020-11-14 01:10:09 +0900

    불필요한 스크립트 제거

--- utils/create_iam_admin_user.sh
+++ utils/create_iam_admin_user.sh
@@ -13,7 +13,6 @@ ADMIN_POLICY_NAME="arn:aws:iam::aws:policy/AdministratorAccess"
 echo "attach ${ADMIN_POLICY_NAME} to ${USER_NAME}"
 aws iam attach-user-policy --user-name ${USER_NAME} --policy-arn ${ADMIN_POLICY_NAME} --output text > /dev/null
 
-aws iam list-attached-user-policies --user-name ${USER_NAME} --output text > /dev/null
 read -ra ACCESS_INFO <<< "$(aws iam create-access-key --user-name ${USER_NAME} --query "AccessKey.[AccessKeyId,SecretAccessKey]" --output text)"
 ACCESS_KEY_ID=${ACCESS_INFO[0]}
 SECRET_ACCESS_KEY=${ACCESS_INFO[1]}
