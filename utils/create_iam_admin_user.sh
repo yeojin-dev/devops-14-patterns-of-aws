53125aba34ebef7aa33940af01271cf6f1808fa2
Yeojin Kim <yeojin.dev@gmail.com>
2020-11-14 01:02:43 +0900

    AWS 어드민 유저 생성 후 credentials 파일에 저장

--- utils/create_iam_admin_user.sh
+++ utils/create_iam_admin_user.sh
@@ -14,4 +14,21 @@ echo "attach ${ADMIN_POLICY_NAME} to ${USER_NAME}"
 aws iam attach-user-policy --user-name ${USER_NAME} --policy-arn ${ADMIN_POLICY_NAME} --output text > /dev/null
 
 aws iam list-attached-user-policies --user-name ${USER_NAME} --output text > /dev/null
-aws iam create-access-key --user-name ${USER_NAME} | jq .
+read -ra ACCESS_INFO <<< "$(aws iam create-access-key --user-name ${USER_NAME} --query "AccessKey.[AccessKeyId,SecretAccessKey]" --output text)"
+ACCESS_KEY_ID=${ACCESS_INFO[0]}
+SECRET_ACCESS_KEY=${ACCESS_INFO[1]}
+
+{
+  echo "[${USER_NAME}]"
+  echo "aws_access_key_id = ${ACCESS_KEY_ID}"
+  echo "aws_secret_access_key = ${SECRET_ACCESS_KEY}"
+} >> ~/.aws/credentials
+
+printf "\n"
+echo "[${USER_NAME}]"
+echo "aws_access_key_id = ${ACCESS_KEY_ID}"
+echo "aws_secret_access_key = ${SECRET_ACCESS_KEY}"
+printf "\n"
+echo "write the credential below in ~/.aws/credentials"
+
+echo 'finish'
