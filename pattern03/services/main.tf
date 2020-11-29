6461457bf4cd04dd93fe7bdc339430b7f534cd01
Yeojin Kim <yeojin.dev@gmail.com>
2020-11-29 21:39:20 +0900

    패턴03: Auto Scaling 생성 후 ALB 리스너 추가

--- pattern03/services/main.tf
+++ pattern03/services/main.tf
@@ -1,3 +1,45 @@
+resource "aws_launch_template" "intra" {
+  name = "intra-web-app"
+  instance_type = "t2.micro"
+  image_id = data.aws_ami.intra_web_app.id
+  vpc_security_group_ids = [aws_security_group.intra_instance.id]
+}
+
+data "aws_ami" "intra_web_app" {
+  owners = ["self"]
+  name_regex = "14-patterns-of-aws-02"
+  most_recent = true
+}
+
+resource "aws_autoscaling_group" "intra" {
+  max_size = 2
+  min_size = 1
+  desired_capacity = 1
+  vpc_zone_identifier = [var.subnet_a_id, var.subnet_b_id]
+  target_group_arns = [aws_lb_target_group.intra.arn]
+  launch_template {
+    id = aws_launch_template.intra.id
+    version = "$Latest"
+  }
+}
+
+resource "aws_lb_target_group" "intra" {
+  name = "intra-tg"
+  port = 80
+  protocol = "HTTP"
+  target_type = "instance"
+  vpc_id = var.vpc_id
+}
+
+resource "aws_lb_listener" "intra" {
+  load_balancer_arn = aws_lb.intra.arn
+  port = 80
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.intra.arn
+  }
+}
+
 resource "aws_lb" "intra" {
   name = "intra-alb"
   internal = true
